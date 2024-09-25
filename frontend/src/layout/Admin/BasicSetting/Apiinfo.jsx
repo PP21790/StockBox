e6530d14368e5c91b649c5e28f249b99ace89c7f
ajay90989 <ajay.office.patidar>
2024-09-25 15:52:19 +0530

    done

--- frontend/src/layout/Admin/BasicSetting/Apiinfo.jsx
+++ frontend/src/layout/Admin/BasicSetting/Apiinfo.jsx
@@ -3,8 +3,181 @@ import React from 'react'
 const Apiinfo = () => {
   return (
     <div>
-      zfdds
+    <div className="page-content">
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-3 row-cols-xl-3 justify-content-center">
+        {/* <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div>
+                <h5 className="card-title">Special title treatment</h5>
+              </div>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional content.
+              </p>{" "}
+              <a href="javascript:;" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div> */}
+        {/* <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div>
+                <h5 className="card-title">Special title treatment</h5>
+              </div>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional content.
+              </p>{" "}
+              <a href="javascript:;" className="btn btn-danger">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div> */}
+        {/* <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div>
+                <h5 className="card-title">Special title treatment</h5>
+              </div>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional content.
+              </p>{" "}
+              <a href="javascript:;" className="btn btn-success">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div> */}
+        <div className="col">
+          <div className="card">
+            <div className="card-header mt-2">
+              <h5>Digio</h5></div>
+            <div className="card-body mt-2">
+              <form className="row g-3 mt-2 mb-3">
+                <div className="row">
+
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                      Digio Client
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                    Digio Client Secret
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="card-footer text-center">
+
+              <button type="button" className="btn btn-primary mb-2">
+                Update
+              </button></div>
+          </div>
+        </div>
+        {/* <div className="col">
+          <div className="card">
+            <div className="card-header mt-2"><h5>Razorpay</h5></div>
+            <div className="card-body mt-2">
+              <form className="row g-3 mt-2 mb-3">
+                <div className="row">
+
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                      Razorpay Key
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                      Razorpay Status
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="card-footer text-center">
+
+              <button type="button" className="btn btn-primary mb-2">
+                Update
+              </button></div>
+          </div>
+        </div> */}
+        {/* <div className="col">
+          <div className="card">
+            <div className="card-header mt-2"><h5>Razorpay</h5></div>
+            <div className="card-body mt-2">
+              <form className="row g-3 mt-2 mb-3">
+                <div className="row">
+
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                      Razorpay Key
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+
+                  <div className="col-md-12 mb-2">
+                    <label htmlFor="input3" className="form-label">
+                      Razorpay Status
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="input3"
+
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="card-footer text-center">
+
+              <button type="button" className="btn btn-primary mb-2">
+                Update
+              </button></div>
+          </div>
+        </div> */}
+      </div>
+
     </div>
+  </div>
   )
 }
 
