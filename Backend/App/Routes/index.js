dd8b9a92bffb7b6a4dc41f0ed0921e744bcd5c03
PP21790 <pankajpatidar333@gmail.com>
2024-10-07 16:45:54 +0530

    done

--- Backend/App/Routes/index.js
+++ Backend/App/Routes/index.js
@@ -20,6 +20,7 @@ module.exports = function (app) {
     app.use(require("./Mailtemplate"))
     app.use(require("./Angle"))
     app.use(require("./Aliceblue"))
+    app.use(require("./Broadcast"))
 
 
 
