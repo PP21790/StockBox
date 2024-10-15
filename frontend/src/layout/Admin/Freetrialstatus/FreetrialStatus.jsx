bfa2e2dd13e8649f99afa85a6649f39bb80afc10
himanshukubde <himanshukd25@gmail.com>
2024-10-15 14:26:34 +0530

    wff

--- frontend/src/layout/Admin/Freetrialstatus/FreetrialStatus.jsx
+++ frontend/src/layout/Admin/Freetrialstatus/FreetrialStatus.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { addfreeClient, basicsettinglist } from '../../../Services/Admin';
 import Swal from 'sweetalert2';
-
-
+import { Link } from 'react-router-dom';
+import Table from '../../../components/Table';
 
 const FreetrialStatus = () => {
   const token = localStorage.getItem('token');
- 
-  
+
+
   const [data, setData] = useState([]);
   const [addStatus, setAddStatus] = useState({
     id: "",
-    freetrial: "" 
+    freetrial: ""
   });
 
 
@@ -42,7 +42,7 @@ const FreetrialStatus = () => {
         freetrial: addStatus.freetrial
       };
 
-      const response = await addfreeClient(data,token);
+      const response = await addfreeClient(data, token);
       if (response?.status) {
         Swal.fire({
           icon: 'success',
@@ -51,8 +51,8 @@ const FreetrialStatus = () => {
           timer: 1500,
           timerProgressBar: true,
         });
-  
-        getApidetail(); 
+
+        getApidetail();
       }
     } catch (error) {
       Swal.fire({
@@ -75,44 +75,84 @@ const FreetrialStatus = () => {
 
 
 
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#f5f5f5',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    padding: '20px',
-  };
 
-  const selectStyle = {
-    padding: '10px',
-    marginTop: '15px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    fontSize: '16px',
-    width: '150px',
-  };
+  const columns = [
+    {
+      name: 'S.No',
+      selector: (row, index) => index + 1,
+      sortable: false,
 
-  const buttonStyle = {
-    padding: '10px 15px',
-    marginTop: '20px',
-    backgroundColor: '#007bff',
-    color: 'white',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    fontSize: '16px',
-  };
+    },
+    {
+      name: 'Title',
+      selector: row => row.title,
+      sortable: true,
+    },
+    // {
+    //     name: 'Active Status',
+    //     selector: row => (
+    //         <div className="form-check form-switch form-check-info">
+    //             <input
+    //                 id={`rating_${row.status}`}
+    //                 className="form-check-input toggleswitch"
+    //                 type="checkbox"
+    //                 checked={row.status === true}
+    //                 onChange={(event) => handleSwitchChange(event, row._id)}
+    //             />
+    //             <label
+    //                 htmlFor={`rating_${row.status}`}
+    //                 className="checktoggle checkbox-bg"
+    //             ></label>
+    //         </div>
+    //     ),
+    //     sortable: true,
+    // },
+    {
+      name: 'Created At',
+      selector: row => new Date(row.created_at).toLocaleDateString(),
+      sortable: true,
+    },
+    {
+      name: 'Updated At',
+      selector: row => new Date(row.updated_at).toLocaleDateString(),
+      sortable: true,
+    },
+
+    // {
+    //     name: 'Actions',
+    //     cell: row => (
+    //         <>
+    //             <div>
+    //                 <SquarePen
+    //                     onClick={() => {
+    //                         setModel(true);
+    //                         setServiceid(row);
+    //                         setUpdatetitle({ title: row.title, id: row._id });
+    //                     }}
+    //                 />
+    //             </div>
+    //             {/* <div>
+    //                 <Trash2 onClick={() => DeleteService(row._id)} />
+    //             </div> */}
+    //         </>
+    //     ),
+    //     ignoreRowClick: true,
+    //     allowOverflow: true,
+    //     button: true,
+    // }
+  ];
+
+
+
+
 
 
 
 
 
   return (
-    <div style={containerStyle}>
+    <div>
+      {/* <div style={containerStyle}>
       <h2>Free Trial Status</h2>
       <select style={selectStyle} value={addStatus.freetrial} onChange={handleSelectChange}>
         <option value="" disabled>
@@ -127,6 +167,131 @@ const FreetrialStatus = () => {
       <button style={buttonStyle} onClick={UpdateClientstatus}>
         Update
       </button>
+    </div> */}
+
+
+      <div className="page-content">
+
+        <div className="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+          <div className="breadcrumb-title pe-3">Free Trial Status</div>
+          <div className="ps-3">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb mb-0 p-0">
+                <li className="breadcrumb-item">
+                  <Link to="/admin/dashboard">
+                    <i className="bx bx-home-alt" />
+                  </Link>
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        <hr />
+
+        <div className="card">
+          <div className="card-body">
+            <div>
+              <label htmlFor="" className='mb-1'> Select Free Days Trial</label>
+              <div className='col-lg-4 d-flex '>
+
+                <select class="form-select" value={addStatus.freetrial} onChange={handleSelectChange}>
+                  <option value="" disabled>
+                    Select days
+                  </option>
+                  {[...Array(7)].map((_, index) => (
+                    <option key={index + 1} value={index + 1}>
+                      {index + 1} day{index > 0 ? 's' : ''}
+                    </option>
+                  ))}
+                </select>
+                <button className='btn btn-primary ms-2' onClick={UpdateClientstatus}>
+                  Update
+                </button>
+
+              </div>
+              <div className="ms-auto">
+                {/* <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal"
+                                >
+                                    <i className="bx bxs-plus-square" />
+                                    Add Service
+                                </button> */}
+
+                <div
+                  className="modal fade"
+                  id="exampleModal"
+                  tabIndex={-1}
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                          Add Service
+                        </h5>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        />
+                      </div>
+                      <div className="modal-body">
+                        <form>
+                          <div className="row">
+                            <div className="col-md-12">
+                              <label htmlFor="">Title</label>
+                              <input
+                                className="form-control mb-3"
+                                type="text"
+                                placeholder='Enter Service Title'
+
+                              />
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+
+                        >
+                          Save
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+              </div>
+            </div>
+            <div className="table-responsive mt-5">
+              <Table
+                columns={columns}
+
+                pagination
+                striped
+                highlightOnHover
+                dense
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
