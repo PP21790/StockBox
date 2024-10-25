42eb82c92d47df202c99ebf6acb677bf1bb36505
PP21790 <pankajpatidar333@gmail.com>
2024-10-25 12:58:55 +0530

    done

--- Backend/App/Controllers/Pushnotification.js
+++ Backend/App/Controllers/Pushnotification.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../template/stockbox-15e55-firebase-adminsdk-1zz93-f32c384cb8.json');
+const serviceAccount = require("../../template/stockbox-15e55-firebase-adminsdk-1zz93-b45e3b0c77.json");
+//const serviceAccount = require('../../template/stockbox-15e55-firebase-adminsdk-1zz93-f32c384cb8.json');
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
