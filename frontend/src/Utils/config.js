919291726330d415e3ff8f6cc0916a4a802ab7de
ajay90989 <ajay.office.patidar>
2024-12-30 16:59:42 +0530

    done

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -8,5 +8,5 @@ export const base_url = `http://localhost:5001/`;
 // for image 
 
 export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
-// export const image_baseurl = `${window.location.origin}/`;C
+// export const image_baseurl = `${window.location.origin}/`;
 // 
