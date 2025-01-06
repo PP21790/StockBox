25f2acb624bd731bedd4ac93763d27a990a978be
ajay90989 <ajay.office.patidar>
2025-01-06 10:52:44 +0530

    done

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,14 +1,14 @@
-export const base_url = `http://localhost:5001/`;
+// export const base_url = `http://localhost:5001/`;
 // export const base_url = `http://localhost:5001/`;
 
 
-// export const base_url = `${window.location.origin}/backend/`
+export const base_url = `${window.location.origin}/backend/`
 
 
 // for image
 
-export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
-// export const image_baseurl = `${window.location.origin}/`;
+// export const image_baseurl = `https://stockboxpnp.pnpuniverse.com/`;
+export const image_baseurl = `${window.location.origin}/`;
 // 
 
 
